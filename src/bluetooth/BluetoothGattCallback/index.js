a613e191727cb715b958a4ec5581b84f0dcbcc3a
nemanjan00 <nemanjan00@gmail.com>
2020-07-27 22:16:06 +0200

    Gatt

--- src/bluetooth/BluetoothGattCallback/index.js
+++ src/bluetooth/BluetoothGattCallback/index.js
@@ -2,31 +2,67 @@ const BluetoothGattCallback = Java.use("h.a.j.a.a");
 const BluetoothGattCharacteristic = Java.use("android.bluetooth.BluetoothGattCharacteristic");
 // const String = Java.use("java.lang.String");
 
+const buffersIn = {};
+const buffers = {};
+
 const onCharacteristicRead = BluetoothGattCallback.onCharacteristicRead;
 const onCharacteristicWrite = BluetoothGattCallback.onCharacteristicWrite;
+const onCharacteristicChanged = BluetoothGattCallback.onCharacteristicChanged;
 
-onCharacteristicRead.implementation = function(gatt, characteristic, i) {
+onCharacteristicRead.implementation = function(gatt, characteristic, status) {
 	console.log(123);
 
 	const value = BluetoothGattCharacteristic.getValue.call(characteristic);
 
 	console.log(value);
 
-	onCharacteristicRead.call(this, gatt, characteristic, i);
+	onCharacteristicRead.call(this, gatt, characteristic, status);
 };
 
-onCharacteristicWrite.implementation = function(gatt, characteristic, status) {
-	console.log(123);
-
+onCharacteristicChanged.implementation = function(gatt, characteristic) {
 	const value = BluetoothGattCharacteristic.getValue.call(characteristic);
 
 	const buffer = Java.array("byte", value);
 
-	for (var i = 0; i < buffer.length; ++i) {
-		console.log(buffer[i]);
+	const uuid = BluetoothGattCharacteristic.getUuid.call(characteristic);
+
+	if (buffersIn[uuid] === undefined) {
+		buffersIn[uuid] = [];
 	}
 
-	console.log(buffer.length);
+	for (var i = 0; i < buffer.length; ++i) {
+		buffersIn[uuid].push(buffer[i]);
+	}
+
+	console.log(`Characteristic ${uuid} changed ${(buffersIn[uuid]).join(" ")}`);
+
+	console.log();
+	console.log();
+	console.log();
+
+	onCharacteristicChanged.call(this, gatt, characteristic);
+};
+
+onCharacteristicWrite.implementation = function(gatt, characteristic, status) {
+	const value = BluetoothGattCharacteristic.getValue.call(characteristic);
+
+	const buffer = Java.array("byte", value);
+
+	const uuid = BluetoothGattCharacteristic.getUuid.call(characteristic);
+
+	if (buffers[uuid] === undefined) {
+		buffers[uuid] = [];
+	}
+
+	for (var i = 0; i < buffer.length; ++i) {
+		buffers[uuid].push(buffer[i]);
+	}
+
+	console.log(`Characteristic ${uuid} written ${(buffers[uuid]).join(" ")}`);
+
+	console.log();
+	console.log();
+	console.log();
 
 	onCharacteristicWrite.call(this, gatt, characteristic, status);
 };
